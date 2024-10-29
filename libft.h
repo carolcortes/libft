f622c7427e059edf07b4dc14095504f864560e94
carolcortes <caroline.ocortes@gmail.com>
2024-10-29 17:20:51 +0000

    feat: strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:19:24 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/10/28 15:55:05 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/10/29 17:05:05 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	*ft_strrchr(char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-// ft_strnstr
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 // ft_atoi
 
 // - With malloc():
