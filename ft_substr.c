f9f8f1d99278603055295f93e04958aff99e6120
Caroline De Oliveira Cortes <cade-oli@c1r4s4.42porto.com>
2024-11-04 12:30:55 +0000

    fix: substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 10:48:06 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/11/04 11:30:42 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:21:00 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,18 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
 	char	*sub;
 
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
+	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
 	if (len > ft_strlen(s + start))
 		len = ft_strlen(s + start);
 	sub = (char *)malloc(len + 1 * sizeof(char));
 	if (!sub)
 		return (NULL);
-	i = -1;
-	while (++i < len)
-	{
-		sub[i] = s[start + i];
-	}
+	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
 
