69d6e2bb420dfdbad22bb6c2c5393bd0444d3844
carolcortes <caroline.ocortes@gmail.com>
2024-11-06 09:54:57 +0000

    feat: itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,18 +6,18 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:42:37 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/11/04 17:02:54 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/11/06 09:53:56 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	int_len(long n)
+int	digit_len(long n)
 {
 	int	count;
 
-	count = 1;
-	if (n < 0)
+	count = 0;
+	if (n <= 0)
 	{
 		n = -n;
 		count++;
@@ -32,11 +32,27 @@ int	int_len(long n)
 
 char	*ft_itoa(int n)
 {
-	char	*a;
-	long	nbr;
-	int		len;
+	const char	*digits = "0123456789";
+	char		*a;
+	int			len;
+	long		nbr;
 
 	nbr = n;
-
+	len = digit_len(nbr);
+	a = (char *)malloc((len + 1) * sizeof(char));
+	if (!a)
+		return (NULL);
+	a[len--] = '\0';
+	if (nbr < 0)
+		nbr = -nbr;
+	while (nbr > 0)
+	{
+		a[len--] = digits[nbr % 10];
+		nbr /= 10;
+	}
+	if (n == 0)
+		a[0] = '0';
+	if (n < 0)
+		a[0] = '-';
 	return (a);
 }
